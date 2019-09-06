47998988e93e2cf163ccf85619066523d6edf62c
Simon Gamble <simong@headuplabs.com>
2019-09-06 21:34:25 +1000

    layout mission card

--- src/components/MissionCard.tsx
+++ src/components/MissionCard.tsx
@@ -10,36 +10,48 @@ import {
 
 import Date from './Date';
 import Title from './Title';
+import Thumbnail from './Thumbnail';
 
 const styles = StyleSheet.create({
   card: {
-    flex: 1,
-    borderBottom: '2',
+    backgroundColor: '#ccc',
     borderColor: '#000000',
     borderRadius: 6,
-    shadowColor: '#e7e7eb',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowRadius: 6,
-    shadowOpacity: 1,
+    marginHorizontal: 12,
+    marginTop: 12,
+    padding: 12,
+    flexDirection: 'row',
+  },
+  cardInfo: {
+    flex: 1,
+  },
+  thumbnail: {
+    marginRight: 10,
   },
 });
 
 interface Props {
   title: string;
   date: string;
+  thumbnail: string;
   style?: StyleProp<ViewStyle>;
   styleText?: StyleProp<TextStyle>;
 }
 
-class MissionCard extends React.Component<Props> {
+class MissionCard extends React.PureComponent<Props> {
   render() {
     return (
       <View style={[styles.card, this.props.style]}>
-        <Title text={this.props.title} />
-        <Date date={this.props.date} format="llll" />
+        <Thumbnail
+          url={this.props.thumbnail}
+          width={50}
+          height={50}
+          style={styles.thumbnail}
+        />
+        <View style={styles.cardInfo}>
+          <Title text={this.props.title} />
+          <Date date={this.props.date} format="llll" />
+        </View>
       </View>
     );
   }
