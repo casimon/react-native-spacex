a51189036871bc6a412edd7ca1ca4c5eaa7d4555
Simon Gamble <simong@headuplabs.com>
2019-09-06 21:32:50 +1000

    Add logo to header title on home screen

---
+++ src/components/LogoTitle.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Image, StyleSheet} from 'react-native';
+
+import images from '../images';
+
+const styles = StyleSheet.create({
+  image: {
+    width: 150,
+    height: 30,
+    tintColor: '#fff',
+  },
+});
+
+class LogoTitle extends React.Component {
+  render() {
+    return (
+      <Image style={styles.image} source={images.logo} resizeMode="contain" />
+    );
+  }
+}
+
+export default LogoTitle;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
-import {Button, View, Text} from 'react-native';
+import {View} from 'react-native';
+import {
+  NavigationParams,
+  NavigationScreenProp,
+  NavigationState,
+} from 'react-navigation';
+
+import LogoTitle from '../components/LogoTitle';
 
 import MissionCard from '../components/MissionCard';
 
-class HomeScreen extends React.Component {
+interface Props {
+  navigation: NavigationScreenProp<NavigationState, NavigationParams>;
+}
+
+class HomeScreen extends React.Component<Props> {
+  static navigationOptions = {
+    headerTitle: <LogoTitle />,
+  };
+
   render() {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <MissionCard title="CRS-17" date="2019-05-04T06:48:00.000Z" />
-        <MissionCard title="CRS-17" date="2019-05-04T06:48:00.000Z" />
-        <MissionCard title="CRS-17" date="2019-05-04T06:48:00.000Z" />
-        <MissionCard title="CRS-17" date="2019-05-04T06:48:00.000Z" />
+      <View style={{flex: 1}}>
+        <MissionCard
+          title="CRS-17"
+          date="2019-05-04T06:48:00.000Z"
+          thumbnail="https://images2.imgbox.com/fc/58/9UErD3ut_o.png"
+        />
+        <MissionCard
+          title="CRS-17"
+          date="2019-05-04T06:48:00.000Z"
+          thumbnail="https://images2.imgbox.com/fc/58/9UErD3ut_o.png"
+        />
+        <MissionCard
+          title="CRS-17"
+          date="2019-05-04T06:48:00.000Z"
+          thumbnail="https://images2.imgbox.com/fc/58/9UErD3ut_o.png"
+        />
+        <MissionCard
+          title="CRS-17"
+          date="2019-05-04T06:48:00.000Z"
+          thumbnail="https://images2.imgbox.com/fc/58/9UErD3ut_o.png"
+        />
       </View>
     );
   }
