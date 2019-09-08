e68fd10fc2ff5149202d895cb8efd7ff5cab278c
gamsim <simong@headuplabs.com>
2019-09-08 21:21:54 +1000

    hide yellowbox warnings

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  */
 
 import {AppRegistry} from 'react-native';
+import {YellowBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+YellowBox.ignoreWarnings(['Warning:']);
+YellowBox.ignoreWarnings(['Remote']);
 
 AppRegistry.registerComponent(appName, () => App);
